fc3308683010335544733890b3a278147edaf373
Marc Pont <mpont91@gmail.com>
2022-03-04 08:43:55 +0100

    Add vuetify instance to tests. But keep failing

--- src/components/__tests__/HelloWorld.spec.js
+++ src/components/__tests__/HelloWorld.spec.js
@@ -1,11 +1,20 @@
-import { describe, it, expect } from 'vitest'
-
-import { mount } from '@vue/test-utils'
+import {describe, it, expect} from 'vitest'
+import {createVuetify} from "vuetify"
+import * as components from "vuetify/components"
+import * as directives from "vuetify/directives"
+import {mount} from '@vue/test-utils'
 import HelloWorld from '../HelloWorld.vue'
 
 describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
-  })
+    const vuetify = createVuetify({components, directives})
+
+    it('renders properly', () => {
+        const wrapper = mount(HelloWorld, {
+            global: {
+                plugins: [vuetify],
+            },
+            props: { title: "Hello Vitest" },
+        })
+        expect(wrapper.text()).toContain('Hello Vitest')
+    })
 })
